9bf6a1ccba5f28556732a8c8e0360832a10c1d04
wayne <eliutov22@gmail.com>
2024-11-13 20:41:16 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.show = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tbRequest = new System.Windows.Forms.TextBox();
+            this.btAsync = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(591, 44);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 23);
+            this.show.TabIndex = 0;
+            this.show.Text = "show";
+            this.show.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(75, 163);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(477, 247);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // tbRequest
+            // 
+            this.tbRequest.Location = new System.Drawing.Point(75, 45);
+            this.tbRequest.Name = "tbRequest";
+            this.tbRequest.Size = new System.Drawing.Size(477, 22);
+            this.tbRequest.TabIndex = 2;
+            // 
+            // btAsync
+            // 
+            this.btAsync.Location = new System.Drawing.Point(702, 43);
+            this.btAsync.Name = "btAsync";
+            this.btAsync.Size = new System.Drawing.Size(75, 23);
+            this.btAsync.TabIndex = 3;
+            this.btAsync.Text = "Async callback";
+            this.btAsync.UseVisualStyleBackColor = true;
+            this.btAsync.Click += new System.EventHandler(this.btAsync_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(853, 470);
+            this.Controls.Add(this.btAsync);
+            this.Controls.Add(this.tbRequest);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.show);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox tbRequest;
+        private System.Windows.Forms.Button btAsync;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
+using System;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private SqlDataReader reader;
+        private DataTable table;
+        private SqlConnection conn;
+        string cs = "";
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            cs = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+            conn.ConnectionString = cs;
+        }
+        private void show_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                SqlCommand comm = new SqlCommand();
+                comm.CommandText = tbRequest.Text;
+                comm.Connection = conn;
+                dataGridView1.DataSource = null;
+                conn.Open();
+                table = new DataTable();
+                reader = comm.ExecuteReader();
+                int line = 0;
+                do
+                {
+                    while (reader.Read())
+                    {
+                        if (line == 0)
+                        {
+                            for (int i = 0; i <
+                            reader.FieldCount; i++)
+                            {
+                                table.Columns.Add(reader.
+                                GetName(i));
+                            }
+                            line++;
+                        }
+                        DataRow row = table.NewRow();
+                        for (int i = 0; i <
+                        reader.FieldCount; i++)
+                        {
+                            row[i] = reader[i];
+                        }
+                        table.Rows.Add(row);
+                    }
+                } while (reader.NextResult());
+                dataGridView1.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Probably wrong request syntax");
+            }
+
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+        private void btAsync_Click(object sender, EventArgs e)
+        {
+            /// блок 1
+            const string AsyncEnabled = "Asynchronous Processing=true";
+            if (!cs.Contains(AsyncEnabled))
+            {
+                cs = String.Format("{0}; {1}", cs, AsyncEnabled);
+            }
+            ///
+            conn = new SqlConnection(cs);
+            SqlCommand comm = conn.CreateCommand();
+            /// блок 2
+            comm.CommandText = "WAITFOR DELAY /00:00:05/; SELECT * FROM Books; ";
+            comm.CommandType = CommandType.Text;
+            comm.CommandTimeout = 30;
+            ///
+            try
+            {
+                conn.Open();
+                /// блок 3
+                AsyncCallback callback = new AsyncCallback(GetDataCallback);
+                comm.BeginExecuteReader(callback, comm);
+                MessageBox.Show("Added thread is working...");
+                ///
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void GetDataCallback(IAsyncResult result)
+        {
+            SqlDataReader reader = null;
+            try
+            {
+                /// блок 1
+                SqlCommand command = (SqlCommand)result.AsyncState;
+                ///
+                /// блок 2
+                reader = command.EndExecuteReader(result);
+                ///
+                table = new DataTable();
+                int line = 0;
+                do
+                {
+                    while (reader.Read())
+                    {
+                        if (line == 0)
+                        {
+                            for (int i = 0; i <
+                            reader.FieldCount; i++)
+                            {
+                                table.Columns.Add(reader.GetName(i));
+                            }
+                            line++;
+                        }
+                        DataRow row = table.NewRow();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            row[i] = reader[i];
+                        }
+                        table.Rows.Add(row);
+                    }
+                } while (reader.NextResult());
+                DgvAction();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("From Callback 1:" + ex.Message);
+            }
+            finally
+            {
+                try
+                {
+                    if (!reader.IsClosed)
+                    {
+                        reader.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("From Callback 2:" + ex.Message);
+                }
+            }
+        }
+        private void DgvAction()
+        {
+            if (dataGridView2.InvokeRequired)
+            {
+                dataGridView2.Invoke(new Action(DgvAction));
+                return;
+            }
+            dataGridView2.DataSource = table;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
